0d3d031b2ff1b054eea342e3dbb727d68be5b229
Hugo Angeles <hugoangeles0810@gmail.com>
2015-01-22 16:58:29 -0500

    servicios

--- mikhuna/src/main/java/com/jasoftsolutions/mikhuna/util/StringUtil.java
+++ mikhuna/src/main/java/com/jasoftsolutions/mikhuna/util/StringUtil.java
@@ -1,5 +1,9 @@
 package com.jasoftsolutions.mikhuna.util;
 
+import com.jasoftsolutions.mikhuna.model.Service;
+
+import java.util.List;
+
 /**
  * Created by pc07 on 04/04/2014.
  */
@@ -7,6 +11,20 @@ public class StringUtil {
 
     public static String currencyFormat(String currency, Number amount) {
         return currency + " " + amount.toString();
+    }
+
+    public static String getServicesOfRestaurant(List<Service> services){
+        StringBuilder sb = new StringBuilder();
+        int len = services.size();
+        Service s = null;
+        for (int i=0; i<len-1; i++){
+            s  = services.get(i);
+            sb.append(s.getName() + ", ");
+        }
+        s = services.get(len-1);
+        sb.append(s.getName()+".");
+
+        return sb.toString();
     }
 
 
